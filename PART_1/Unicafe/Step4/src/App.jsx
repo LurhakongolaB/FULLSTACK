ee28c3f84ff31405e50b871ae43662f3fbdace6a
Lurhakongola <lurhakongola@gmail.com>
2026-03-19 17:02:41 +0300

    Unicafe Stap Four Completed

---
+++ PART_1/Unicafe/Step4/src/App.jsx
@@ -0,0 +1,51 @@
+import { useState } from 'react'
+
+const Statistics = (props) => {
+  const { good, neutral, bad } = props
+  const all = good + neutral + bad
+
+  console.log("Statistics check - total feedback:", all)
+
+  if (all === 0) {
+    return (
+      <div>
+        <h2>statistics</h2>
+        <p>No feedback given</p>
+      </div>
+    )
+  }
+  const average = (good - bad) / all
+  const positive = (good / all) * 100
+
+  return (
+    <div>
+      <h2>statistics</h2>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>positive {positive} %</p>
+    </div>
+  )
+}
+
+const App = () => {
+  console.log('redering')
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <button onClick={() => setGood(good + 1)}>good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+      <button onClick={() => setBad(bad + 1)}>bad</button>
+      
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  )
+}
+
+export default App
