33bd51f92497acbbf340b3b12cc7cd28999a24ab
Lurhakongola <lurhakongola@gmail.com>
2026-04-06 14:18:21 +0300

    completed phone book step1

---
+++ PART_2/PhoneBook/src/App.jsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    console.log('Form submitted. Current newName state:', newName)
+
+    const nameObject = {
+      name: newName
+    }
+
+    setPersons(persons.concat(nameObject))
+    setNewName('')
+    console.log('Updated persons list:', persons.concat(nameObject))
+  }
+
+  const handleNameChange = (event) => {
+    console.log('Input value changing to:', event.target.value)
+    setNewName(event.target.value)
+  }
+  return (
+    <div>
+      <h2>Phonebook</h2>
+
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map(person => 
+          <li key={person.name}>{person.name}</li>
+        )}
+      </ul>
+    </div>
+  )
+}
+
+export default App
