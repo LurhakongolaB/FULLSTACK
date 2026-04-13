6f8fef8e9fb00ab9cbb334e93cc86e94dd3d3a55
Lurhakongola <lurhakongola@gmail.com>
2026-04-13 10:53:31 +0300

    completed The Phonebook Step 6, successfully created my db.json server

--- PART_2/PhoneBook/src/App.jsx
+++ PART_2/PhoneBook/src/App.jsx
@@ -1,93 +1,91 @@
-import { useState } from "react";
-
-const Filter =({value, onChange})=>{
-  return (
-    <div>
-        filter shown with <input value={value}  onChange={onChange}/>
-      </div>
-  )
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 
-}
-const PersonForm =(props)=>(
+const Filter = ({ value, onChange }) => (
+  <div>
+    filter shown with <input value={value} onChange={onChange} />
+  </div>
+)
+
+
+const PersonForm = (props) => (
   <form onSubmit={props.addPerson}>
     <div>name: <input value={props.newName} onChange={props.handleNameChange} /></div>
     <div>number: <input value={props.newNumber} onChange={props.handleNumberChange} /></div>
     <div><button type="submit">add</button></div>
   </form>
 )
-const Persons=({personsToShow})=>(
-    <ul>
-        {personsToShow.map(person => 
-          <li key={person.name}>{person.name} {person.number}</li>
-        )}
-      </ul>
-)
 
-const Person = ({person})=>(
-  <li> {person.name} {person.number}</li>  
+const Persons = ({ personsToShow }) => (
+  <ul>
+    {personsToShow.map(person => 
+      <li key={person.id}>{person.name} {person.number}</li>
+    )}
+  </ul>
 )
-
 
 const App = () => {
- const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber]= useState('')
-  const[filterName, setFilterName]= useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [filterName, setFilterName] = useState('')
+
+  useEffect(() => {
+    console.log('Effect: Sending Axios request to port 3001')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('Promise fulfilled: Data is here!')
+        setPersons(response.data)
+      })
+  }, []) 
 
   const addPerson = (event) => {
     event.preventDefault()
-    console.log('Form submitted. Current newName state:', newName)
-
-    if (persons.some(person => person.name === newName)){
-      alert(`${newName} is already added to Phonebook` )
+    if (persons.some(person => person.name === newName)) {
+      alert(`${newName} is already added to phonebook`)
       return
     }
-    
+
     const nameObject = {
       name: newName,
       number: newNumber,
-      id: persons.length +1
+      id: String(persons.length + 1)
     }
 
     setPersons(persons.concat(nameObject))
     setNewName('')
     setNewNumber('')
-    console.log('successfuly added : ', (nameObject))
   }
-  const personsToShow = filterName ===''
-  ? persons
-  : persons.filter(person => person.name.toLocaleLowerCase(). includes(filterName.toLocaleLowerCase()))
- 
-  
+
+  const handleNameChange = (e) => setNewName(e.target.value)
+  const handleNumberChange = (e) => setNewNumber(e.target.value)
+
+  const personsToShow = filterName === ''
+    ? persons
+    : persons.filter(person => 
+        person.name.toLowerCase().includes(filterName.toLowerCase())
+      )
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <Filter value={filterName} onChange={(e)=>setFilterName(e.target.value)}/>
-
+      <Filter value={filterName} onChange={(e) => setFilterName(e.target.value)} />
       
-      <h3>add a new </h3>
-      <PersonForm
-      addPerson={addPerson}
-      newName={newName}
-      handleNameChange={(e) => setNewName(e.target.event)}
-      newNumber={newNumber}
-      handleNumberChange={(e)=> setNewNumber(e.target.value)}
+      <h3>Add a new</h3>
+      <PersonForm 
+        addPerson={addPerson} 
+        newName={newName} 
+        handleNameChange={handleNameChange}
+        newNumber={newNumber}
+        handleNumberChange={handleNumberChange}
       />
-      
 
       <h3>Numbers</h3>
-      <Persons personsToShow={personsToShow}/>
-    
+      <Persons personsToShow={personsToShow} />
     </div>
   )
-  }
-
+}
 
 export default App
