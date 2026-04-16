01cce592df4f4a661109fd83a919a8f1582e4384
Lurhakongola <lurhakongola@gmail.com>
2026-04-16 09:19:20 +0300

    rectified country information, step1

--- PART_2/CountryInformation/src/App.jsx
+++ PART_2/CountryInformation/src/App.jsx
@@ -6,38 +6,62 @@ const App = () => {
   const [allCountries, setAllCountries] = useState([])
 
   useEffect(() => {
+    console.log('Fetching all countries...')
     axios
       .get('https://studies.cs.helsinki.fi/restcountries/api/all')
       .then(response => {
+        console.log('Data received! Total countries:', response.data.length)
         setAllCountries(response.data)
       })
-  }, []) 
-
+  }, [])
 
   const countriesToShow = allCountries.filter(c => 
     c.name.common.toLowerCase().includes(query.toLowerCase())
   )
+
+  console.log('Query:', query, '| Matches found:', countriesToShow.length)
 
   return (
     <div>
       find countries <input value={query} onChange={(e) => setQuery(e.target.value)} />
       
       <div>
-      
-        {countriesToShow.length > 10 ? (
+        {countriesToShow.length > 10 && (
           <p>Too many matches, specify another filter</p>
-        ) : countriesToShow.length > 1 ? (
+        )}
+
+        {countriesToShow.length <= 10 && countriesToShow.length > 1 && (
           countriesToShow.map(c => <div key={c.name.common}>{c.name.common}</div>)
-        ) : countriesToShow.length === 1 ? (
-          <div>
-            <h1>{countriesToShow[0].name.common}</h1>
-            <p>capital {countriesToShow[0].capital}</p>
-            <p>area {countriesToShow[0].area}</p>
-          </div>
-        ) : (
-          <p>No matches found</p>
+        )}
+
+        {countriesToShow.length === 1 && (
+          <CountryDetail country={countriesToShow[0]} />
         )}
       </div>
+    </div>
+  )
+}
+
+const CountryDetail = ({ country }) => {
+  console.log('Rendering details for:', country.name.common)
+  
+  return (
+    <div>
+      <h1>{country.name.common}</h1>
+      <p>capital {country.capital[0]}</p>
+      <p>area {country.area}</p>
+
+      <h3>Languages</h3>
+      <ul>
+        {Object.values(country.languages).map(lang => (
+          <li key={lang}>{lang}</li>
+        ))}
+      </ul>
+      <img 
+        src={country.flags.png} 
+        alt={country.name.common} 
+        width="150" 
+      />
     </div>
   )
 }
