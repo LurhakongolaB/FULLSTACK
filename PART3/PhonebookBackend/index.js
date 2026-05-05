9a01f0ab81c1eb6738a506f645e09f1040a2d95d
Lurhakongola <lurhakongola@gmail.com>
2026-05-05 10:18:51 +0300

    Final fix for folder visibility

---
+++ PART3/PhonebookBackend/index.js
@@ -0,0 +1,113 @@
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const app = express()
+
+app.use(express.json())
+/
+morgan.token('body', (req, res) => {
+  return JSON.stringify(req.body)
+})
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
+let persons = [
+    {
+        "id": "1",
+        "name": "Saima Hellas",
+        "number": "040-123456"
+    },
+
+    { 
+      "id": "2",
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": "3",
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": "4",
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    },
+    { 
+      "id": "5",
+      "name": "Marian", 
+      "number": "39-21-6423127"
+    },
+    { 
+      "id": "6",
+      "name": "Zaina", 
+      "number": "39-21-6423127"
+    }
+
+]
+
+app.get('/', (req, res) => {
+  res.send('<h1>Phonebook Backend is running!</h1>');
+});
+app.get('/api/persons', (req, res) => {
+  res.json(persons)
+})
+
+app.get('/info', (req, res) =>{
+    const entries = persons.length
+    const date = new Date()
+
+    res.send(` <p>Phonebood has infor for ${entries}</p>  <p>${date}</p>
+        `)
+
+})
+
+app.get('/api/persons/:id', (req, res) =>{
+    const id =req.params.id    
+    const person = persons.find(person => person.id === id)
+    if(person) {
+        res.json(person)
+    } else {res.status(404).end()}
+})
+
+
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  persons = persons.filter(p => p.id !== id)
+  response.status(204).end()
+}) 
+
+// adding data to the site
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({ 
+      error: 'name or number missing' 
+    })
+  }
+
+  const nameExists = persons.find(p => p.name === body.name)
+  if (nameExists) {
+    return response.status(400).json({ 
+      error: 'name must be unique' 
+    })
+  }
+
+  const person = {
+    id: Math.floor(Math.random() * 100000).toString(), 
+    name: body.name,
+    number: body.number,
+  }
+
+  persons = persons.concat(person)
+
+  response.json(person)
+})
+// Port that the server will listen on, either from environment variable or default to 3001
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
+
