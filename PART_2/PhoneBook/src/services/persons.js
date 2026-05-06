6aa384602ad120bd5f0097d8d438e60d6f96e6d6
Lurhakongola <lurhakongola@gmail.com>
2026-05-06 03:52:46 +0300

    Build created

--- PART_2/PhoneBook/src/services/persons.js
+++ PART_2/PhoneBook/src/services/persons.js
@@ -1,24 +1,33 @@
 import axios from 'axios'
 
-// If VITE_BACKEND_URL is defined (on Render), use it. 
-// Otherwise, use the relative path (for local proxy).
-const backendUrl = import.meta.env.VITE_BACKEND_URL || ''
-const baseUrl = `${backendUrl}/api/persons`
+const baseUrl = '/api/persons'
 
 const getAll = () => {
-  return axios.get(baseUrl).then(res => res.data)
+  return axios.get(baseUrl).then(res => {
+    console.log('GET successful')
+    return res.data
+  })
 }
 
 const create = newObject => {
-  return axios.post(baseUrl, newObject).then(res => res.data)
+  return axios.post(baseUrl, newObject).then(res => {
+    console.log('POST successful')
+    return res.data
+  })
 }
 
 const update = (id, newObject) => {
-  return axios.put(`${baseUrl}/${id}`, newObject).then(res => res.data)
+  return axios.put(`${baseUrl}/${id}`, newObject).then(res => {
+    console.log('PUT successful')
+    return res.data
+  })
 }
 
 const remove = id => {
-  return axios.delete(`${baseUrl}/${id}`).then(res => res.data)
+  return axios.delete(`${baseUrl}/${id}`).then(res => {
+    console.log('DELETE successful')
+    return res.data
+  })
 }
 
 export default { getAll, create, update, remove }
