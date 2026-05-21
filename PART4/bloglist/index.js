6c31dbe522cd887ba779a791e0ad7c6964b01701
Lurhakongola <lurhakongola@gmail.com>
2026-05-21 20:22:14 +0300

    completed blog list, step2

--- PART4/bloglist/index.js
+++ PART4/bloglist/index.js
@@ -1,56 +1,7 @@
-require('dotenv').config()
-const express = require('express')
-const mongoose = require('mongoose')
+const app = require('./app') // the actual Express application
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-const app = express()
-app.use(express.json())
-
-// MongoDB connection
-const url = process.env.MONGODB_URI
-
-console.log('connecting to MongoDB...')
-
-mongoose.connect(url)
-  .then(() => {
-    console.log('connected to MongoDB')
-  })
-  .catch(err => {
-    console.log('error connecting to MongoDB:', err.message)
-  })
-
-// schema and model for blogs
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-})
-
-const Blog = mongoose.model('Blog', blogSchema) 
-
-// GET methot all blogs
-
-app.get('/api/blogs', (req, res) => {
-  Blog.find({}).then(blogs => {
-    res.json(blogs)
-  })
-})
-
-
-//  ➕ POST new blog
-
-app.post('/api/blogs', (req, res) => {
-  const blog = new Blog(req.body)
-
-  blog.save().then(saved => {
-    res.json(saved)
-  })
-})
-
-// 🚀 START SERVER
-
-const PORT = process.env.PORT || 3001
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
