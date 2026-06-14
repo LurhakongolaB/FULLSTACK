f1ef79804491cd1094ce40642f29d553868edada
Lurhakongola <lurhakongola@gmail.com>
2026-06-15 00:08:31 +0300

    blog list expansion, step6

--- PART4/bloglist/utils/middleware.js
+++ PART4/bloglist/utils/middleware.js
@@ -1,35 +1,80 @@
 const logger = require('./logger')
+const jwt = require('jsonwebtoken')
+const User = require('../models/user')
+const login = require('../controllers/login')
 
-const requestLogger = (req, res, next) => {
-  logger.info('Method:', req.method)
-  logger.info('Path:', req.path)
+// -------------------- TOKEN EXTRACTOR --------------------
+const tokenExtractor = (request, response, next) => {
+  const authorization = request.get('authorization')
 
-  if (req.body && Object.keys(req.body).length > 0) {
-    logger.info('Body:', req.body)
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    request.token = authorization.substring(7)
+  } else {
+    request.token = null
   }
 
   next()
 }
 
-const unknownEndpoint = (req, res) => {
-  res.status(404).send({ error: 'unknown endpoint' })
+// -------------------- USER EXTRACTOR --------------------
+const userExtractor = async (request, response, next) => {
+  try {
+    const decodedToken = jwt.verify(request.token, process.env.SECRET)
+
+    if (!decodedToken.id) {
+      return response.status(401).json({ error: 'token invalid' })
+    }
+
+    request.user = await User.findById(decodedToken.id)
+    next()
+  } catch (error) {
+    next(error)
+  }
 }
 
-const errorHandler = (err, req, res, next) => {
-  logger.error(err.message)
+// -------------------- REQUEST LOGGER --------------------
+const requestLogger = (request, response, next) => {
+  logger.info('Method:', request.method)
+  logger.info('Path:', request.path)
 
-  if (err.name === 'CastError') {
-    return res.status(400).send({ error: 'malformatted id' })
+  if (request.body && Object.keys(request.body).length > 0) {
+    logger.info('Body:', request.body)
   }
 
-  if (err.name === 'ValidationError') {
-    return res.status(400).json({ error: err.message })
+  next()
+}
+
+// -------------------- UNKNOWN ENDPOINT --------------------
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+// -------------------- ERROR HANDLER --------------------
+const errorHandler = (error, request, response, next) => {
+  logger.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
   }
 
-  next(err)
+  if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+
+  if (error.name === 'JsonWebTokenError') {
+    return response.status(401).json({ error: 'invalid token' })
+  }
+
+  if (error.name === 'TokenExpiredError') {
+    return response.status(401).json({ error: 'token expired' })
+  }
+
+  next(error)
 }
 
 module.exports = {
+  tokenExtractor,
+  userExtractor,
   requestLogger,
   unknownEndpoint,
   errorHandler
