b111f195826121fe889065366ab17db42a550034
Lurhakongola <lurhakongola@gmail.com>
2026-07-15 07:26:33 +0300

    Completed Step2

--- PART_5/bloglist-frontend/src/App.jsx
+++ PART_5/bloglist-frontend/src/App.jsx
@@ -19,6 +19,16 @@ const App = () => {
     fetchBlogs()
   }, [])
 
+  useEffect(() => {
+  const loggedUserJSON =
+    window.localStorage.getItem('loggedBlogappUser')
+
+  if (loggedUserJSON) {
+    const user = JSON.parse(loggedUserJSON)
+    setUser(user)
+  }
+}, [])
+
   const handleLogin = async (event) => {
     event.preventDefault()
 
@@ -29,6 +39,10 @@ const App = () => {
       })
 
       setUser(user)
+      window.localStorage.setItem(
+  'loggedBlogappUser',
+  JSON.stringify(user)
+)
       setUsername('')
       setPassword('')
     } catch (exception) {
@@ -56,11 +70,21 @@ const App = () => {
     )
   }
 
+  const handleLogout = () => {
+    window.localStorage.removeItem('loggedBlogappUser')
+    setUser(null)
+}
+
   return (
     <div>
       <h2>blogs</h2>
 
-      <p>{user.name} logged in</p>
+      <p>
+  {user.name} logged in
+  <button onClick={handleLogout}>
+    logout
+  </button>
+</p>
 
       {blogs.map(blog => (
         <Blog key={blog.id} blog={blog} />
