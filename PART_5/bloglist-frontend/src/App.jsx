438a063542ef90be9f43c638db4b774df6026d2b
Lurhakongola <lurhakongola@gmail.com>
2026-07-15 08:47:28 +0300

    completed Blog List Frontend, step 3

--- PART_5/bloglist-frontend/src/App.jsx
+++ PART_5/bloglist-frontend/src/App.jsx
@@ -5,10 +5,14 @@ import blogService from './services/blogs'
 import loginService from './services/login'
 
 const App = () => {
-  const [blogs, setBlogs] = useState([])
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
-  const [user, setUser] = useState(null)
+const [blogs, setBlogs] = useState([])
+const [username, setUsername] = useState('')
+const [password, setPassword] = useState('')
+const [user, setUser] = useState(null)
+
+const [title, setTitle] = useState('')
+const [author, setAuthor] = useState('')
+const [url, setUrl] = useState('')
 
   useEffect(() => {
     const fetchBlogs = async () => {
@@ -85,6 +89,37 @@ const App = () => {
     logout
   </button>
 </p>
+<h3>Create new blog</h3>
+
+<form>
+  <div>
+    title:
+    <input
+      value={title}
+      onChange={({ target }) => setTitle(target.value)}
+    />
+  </div>
+
+  <div>
+    author:
+    <input
+      value={author}
+      onChange={({ target }) => setAuthor(target.value)}
+    />
+  </div>
+
+  <div>
+    url:
+    <input
+      value={url}
+      onChange={({ target }) => setUrl(target.value)}
+    />
+  </div>
+
+  <button type="submit">
+    create
+  </button>
+</form>
 
       {blogs.map(blog => (
         <Blog key={blog.id} blog={blog} />
